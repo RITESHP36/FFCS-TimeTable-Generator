e1cef203d7264e12b1976ac150c3e85e61fb5d04
RITESHP36 <codingritesh36@gmail.com>
2024-06-26 20:58:53 +0530

    Sharable preset link made and fucntionality added

--- src/components/TimeTableInput.jsx
+++ src/components/TimeTableInput.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCopy } from '@fortawesome/free-solid-svg-icons';
+import { MdContentCopy } from "react-icons/md";
+import { IoShareSocialSharp } from "react-icons/io5";
+
 import { generateTimetables } from "./timetableGenerator";
 
 const encodePreset = (preset) => {
@@ -27,14 +28,31 @@ const TimetableInput = ({ alldata, updateAlldata }) => {
 	const [presetName, setPresetName] = useState("");
 	const [presetCode, setPresetCode] = useState("");
 	const [codeGenerated, setCodeGenerated] = useState(false);
-	const [tooltipText, setTooltipText] = useState('Copy');
+	const [tooltipText, setTooltipText] = useState("Copy");
 	const [isHovered, setIsHovered] = useState(false);
+	const [check, setCheck] = useState(false);
 
 	useEffect(() => {
 		const savedPresets =
 			JSON.parse(localStorage.getItem("timetablePresets")) || [];
 		setPresets(savedPresets);
 	}, []);
+
+	useEffect(() => {
+		const segments = window.location.href.split("/");
+		const presetCodeURL = decodeURIComponent(segments[segments.length - 1]);
+		console.log("Preset code from URL:", presetCodeURL);
+		if (presetCodeURL.length > 0) {
+			setPresetCode(presetCodeURL); // Set the preset code state
+			setCheck(true);
+		}
+	}, []); // Empty dependency array ensures this runs only once on mount
+
+	useEffect(() => {
+		if (check) {
+			loadPresetFromCode();
+		}
+	}, [check]);
 
 	const handleSubjectCountChange = (e) => {
 		const count = parseInt(e.target.value);
@@ -165,189 +183,211 @@ const TimetableInput = ({ alldata, updateAlldata }) => {
 
 	const handleCopyClick = () => {
 		navigator.clipboard.writeText(presetCode);
-		setTooltipText('Copied');
+		setTooltipText("Copied");
 	};
 
-    const handleMouseEnter = () => {
-        setIsHovered(true);
-    };
+	const handleMouseEnter = () => {
+		setIsHovered(true);
+	};
 
-    const handleMouseLeave = () => {
-        setIsHovered(false);
-    };
+	const handleMouseLeave = () => {
+		setIsHovered(false);
+	};
 
 	return (
 		<div className="min-h-screen bg-gray-100 py-10">
 			<div className="max-w-4xl mx-auto px-5">
-				<h1 className="text-4xl font-bold mb-10 text-center text-gray-800">Timetable Generator</h1>
+				<h1 className="text-4xl font-bold mb-10 text-center text-gray-800">
+					Timetable Generator
+				</h1>
 				<div className="bg-white shadow-lg rounded-2xl p-8 mb-10">
-
 					{/* Preset selection and management */}
 					<div className="mb-6 flex justify-between items-center">
 						<div className="w-1/2 pr-2">
-						<label
-							className="block text-gray-700 text-md font-semibold mb-2"
-							htmlFor="preset-select"
-						>
-							Select Preset:
-						</label>
-						<select
-							id="preset-select"
-							className="w-full border border-gray-300 shadow-md rounded-xl py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-							onChange={(e) => loadPreset(presets[e.target.value])}
-						>
-							<option value="">Select a preset</option>
-							{presets.map((preset, index) => (
-							<option key={index} value={index}>
-								{preset.name}
-							</option>
-							))}
-						</select>
+							<label
+								className="block text-gray-700 text-md font-semibold mb-2"
+								htmlFor="preset-select"
+							>
+								Select Preset:
+							</label>
+							<select
+								id="preset-select"
+								className="w-full border border-gray-300 shadow-md rounded-xl py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+								onChange={(e) => loadPreset(presets[e.target.value])}
+							>
+								<option value="">Select a preset</option>
+								{presets.map((preset, index) => (
+									<option key={index} value={index}>
+										{preset.name}
+									</option>
+								))}
+							</select>
 						</div>
 						<div className="w-1/2 pl-2">
-						<label
-							className="block text-gray-700 text-md font-semibold mb-2"
-							htmlFor="preset-name"
-						>
-							Preset Name:
-						</label>
-						<input
-							id="preset-name"
-							className="w-full shadow-md rounded-xl appearance-none border border-gray-300 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-							type="text"
-							value={presetName}
-							onChange={(e) => setPresetName(e.target.value)}
-							placeholder="Enter preset name"
-						/>
+							<label
+								className="block text-gray-700 text-md font-semibold mb-2"
+								htmlFor="preset-name"
+							>
+								Preset Name:
+							</label>
+							<input
+								id="preset-name"
+								className="w-full shadow-md rounded-xl appearance-none border border-gray-300 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+								type="text"
+								value={presetName}
+								onChange={(e) => setPresetName(e.target.value)}
+								placeholder="Enter preset name"
+							/>
 						</div>
 					</div>
 					<div className="mb-6 flex justify-between">
 						<button
-						className="bg-blue-600 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded-xl focus:outline-none focus:shadow-outline"
-						onClick={savePreset}
+							className="bg-blue-600 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded-xl focus:outline-none focus:shadow-outline"
+							onClick={savePreset}
 						>
-						Save as New Preset
+							Save as New Preset
 						</button>
 						<button
-						className="bg-green-600 hover:bg-green-700 text-white font-semibold py-2 px-4 rounded-xl focus:outline-none focus:shadow-outline"
-						onClick={updatePreset}
+							className="bg-green-600 hover:bg-green-700 text-white font-semibold py-2 px-4 rounded-xl focus:outline-none focus:shadow-outline"
+							onClick={updatePreset}
 						>
-						Update Selected Preset
+							Update Selected Preset
 						</button>
 					</div>
 
 					{/* Unique code generator */}
 					<div className="mb-6">
-						<h2 className="text-2xl font-bold mb-4 text-gray-800">Preset Code</h2>
+						<h2 className="text-2xl font-bold mb-4 text-gray-800">
+							Preset Code
+						</h2>
 						<div className="flex items-center">
-						<input
-							className="flex-grow shadow appearance-none border border-gray-300 rounded-xl py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mr-2"
-							type="text"
-							placeholder="Enter preset code"
-							value={presetCode}
-							onChange={(e) => setPresetCode(e.target.value)}
-						/>
-						{codeGenerated && (
-							<div className="relative flex items-center">
-								<FontAwesomeIcon
-									icon={faCopy}
-									className="text-gray-600 h-6 hover:text-gray-700 cursor-pointer mr-4 ml-2 "
-									onClick={handleCopyClick}
-									onMouseEnter={handleMouseEnter}
-									onMouseLeave={handleMouseLeave}
-								/>
-								{isHovered && (
-									<div className="absolute bottom-full mb-2 text-sm bg-gray-800 text-white rounded py-1 px-2">
-										{tooltipText}
-									</div>
-								)}
-							</div>
-						)}
-						<button
-							className="bg-blue-600 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded-xl focus:outline-none focus:shadow-outline mr-2"
-							onClick={generatePresetCode}
-						>
-							Generate Code
-						</button>
-						<button
-							className="bg-green-600 hover:bg-green-700 text-white font-semibold py-2 px-4 rounded-xl focus:outline-none focus:shadow-outline mr-2"
-							onClick={loadPresetFromCode}
-						>
-							Load from Code
-						</button>
+							<input
+								className="flex-grow shadow appearance-none border border-gray-300 rounded-xl py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mr-2"
+								type="text"
+								placeholder="Enter preset code"
+								value={presetCode}
+								onChange={(e) => setPresetCode(e.target.value)}
+							/>
+							{codeGenerated && presetCode.length > 0 && (
+								<div className="relative flex items-center" title="Copy">
+									<MdContentCopy
+										className="text-gray-600 h-6 hover:text-gray-700 cursor-pointer mx-2 "
+										onClick={handleCopyClick}
+									/>
+								</div>
+							)}
+							{codeGenerated && presetCode.length > 0 && (
+								<div className="relative flex items-center" title="Share">
+									<IoShareSocialSharp
+										className="text-gray-600 h-6 hover:text-gray-700 cursor-pointer mx-2"
+										onClick={() => {
+											navigator.clipboard.writeText(
+												window.location.href + presetCode
+											);
+										}}
+									/>
+								</div>
+							)}
+							<button
+								className="bg-blue-600 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded-xl focus:outline-none focus:shadow-outline mr-2"
+								onClick={generatePresetCode}
+							>
+								Generate Code
+							</button>
+							<button
+								className="bg-green-600 hover:bg-green-700 text-white font-semibold py-2 px-4 rounded-xl focus:outline-none focus:shadow-outline mr-2"
+								onClick={loadPresetFromCode}
+							>
+								Load from Code
+							</button>
 						</div>
 					</div>
 
 					{/* Subject count input */}
 					<div className="mb-6">
 						<label
-						className="block text-gray-700 text-md font-semibold mb-2"
-						htmlFor="number-of-subjects"
+							className="block text-gray-700 text-md font-semibold mb-2"
+							htmlFor="number-of-subjects"
 						>
-						Number of Subjects:
+							Number of Subjects:
 						</label>
 						<input
-						className="w-full appearance-none shadow-md border border-gray-300 rounded-xl py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-						id="number-of-subjects"
-						type="number"
-						min="1"
-						value={subjectCount}
-						onChange={handleSubjectCountChange}
+							className="w-full appearance-none shadow-md border border-gray-300 rounded-xl py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+							id="number-of-subjects"
+							type="number"
+							min="1"
+							value={subjectCount}
+							onChange={handleSubjectCountChange}
 						/>
 					</div>
 
 					{/* Subjects and teachers inputs */}
 					{subjects.map((subject, subjectIndex) => (
-						<div key={subjectIndex} className="mb-8 p-6 bg-gray-50 border-4 border-gray-200 rounded-2xl shadow-md">
-						<div className="flex justify-between items-center mb-4">
-							<input
-							className="text-xl font-semibold bg-gray-200 border border-gray-300 rounded-xl py-2 px-3 flex-grow mr-4"
-							type="text"
-							placeholder={`Subject ${subjectIndex + 1}`}
-							value={subject.name}
-							onChange={(e) => handleSubjectNameChange(subjectIndex, e.target.value)}
-							/>
-							<button
-							className="bg-red-600 hover:bg-red-700 text-white font-semibold py-2 px-4 rounded-xl focus:outline-none focus:shadow-outline"
-							onClick={() => removeSubject(subjectIndex)}
-							>
-							Remove Subject
-							</button>
-						</div>
-						{subject.teachers.map((teacher, teacherIndex) => (
-							<div key={teacherIndex} className="mb-4 flex items-center">
-							<input
-								className="flex-grow shadow appearance-none border border-gray-300 rounded-xl py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mr-2"
-								type="text"
-								placeholder="Teacher Name"
-								value={teacher.name}
-								onChange={(e) =>
-								handleTeacherChange(subjectIndex, teacherIndex, "name", e.target.value)
-								}
-							/>
-							<input
-								className="flex-grow shadow appearance-none border border-gray-300 rounded-xl py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mr-2"
-								type="text"
-								placeholder="Slots (comma-separated)"
-								value={teacher.slots}
-								onChange={(e) =>
-								handleTeacherChange(subjectIndex, teacherIndex, "slots", e.target.value)
-								}
-							/>
-							<button
-								className="bg-red-600 hover:bg-red-700 text-white font-semibold py-2 px-4 rounded-xl focus:outline-none focus:shadow-outline"
-								onClick={() => removeTeacher(subjectIndex, teacherIndex)}
-							>
-								Remove
-							</button>
-							</div>
-						))}
-						<button
-							className="bg-blue-600 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded-xl focus:outline-none focus:shadow-outline"
-							onClick={() => addTeacher(subjectIndex)}
+						<div
+							key={subjectIndex}
+							className="mb-8 p-6 bg-gray-50 border-4 border-gray-200 rounded-2xl shadow-md"
 						>
-							Add Teacher
-						</button>
+							<div className="flex justify-between items-center mb-4">
+								<input
+									className="text-xl font-semibold bg-gray-200 border border-gray-300 rounded-xl py-2 px-3 flex-grow mr-4"
+									type="text"
+									placeholder={`Subject ${subjectIndex + 1}`}
+									value={subject.name}
+									onChange={(e) =>
+										handleSubjectNameChange(subjectIndex, e.target.value)
+									}
+								/>
+								<button
+									className="bg-red-600 hover:bg-red-700 text-white font-semibold py-2 px-4 rounded-xl focus:outline-none focus:shadow-outline"
+									onClick={() => removeSubject(subjectIndex)}
+								>
+									Remove Subject
+								</button>
+							</div>
+							{subject.teachers.map((teacher, teacherIndex) => (
+								<div key={teacherIndex} className="mb-4 flex items-center">
+									<input
+										className="flex-grow shadow appearance-none border border-gray-300 rounded-xl py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mr-2"
+										type="text"
+										placeholder="Teacher Name"
+										value={teacher.name}
+										onChange={(e) =>
+											handleTeacherChange(
+												subjectIndex,
+												teacherIndex,
+												"name",
+												e.target.value
+											)
+										}
+									/>
+									<input
+										className="flex-grow shadow appearance-none border border-gray-300 rounded-xl py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mr-2"
+										type="text"
+										placeholder="Slots (comma-separated)"
+										value={teacher.slots}
+										onChange={(e) =>
+											handleTeacherChange(
+												subjectIndex,
+												teacherIndex,
+												"slots",
+												e.target.value
+											)
+										}
+									/>
+									<button
+										className="bg-red-600 hover:bg-red-700 text-white font-semibold py-2 px-4 rounded-xl focus:outline-none focus:shadow-outline"
+										onClick={() => removeTeacher(subjectIndex, teacherIndex)}
+									>
+										Remove
+									</button>
+								</div>
+							))}
+							<button
+								className="bg-blue-600 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded-xl focus:outline-none focus:shadow-outline"
+								onClick={() => addTeacher(subjectIndex)}
+							>
+								Add Teacher
+							</button>
 						</div>
 					))}
 
@@ -358,12 +398,9 @@ const TimetableInput = ({ alldata, updateAlldata }) => {
 					>
 						Generate Timetable
 					</button>
-
 				</div>
 			</div>
 		</div>
-
-
 	);
 };
 
